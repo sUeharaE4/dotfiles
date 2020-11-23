707d615534044e60529d666097fe6f9c9d827cdc
sUeharaE4 <funa1hasi2hasi@gmail.com>
2020-11-24 08:19:01 +0900

    Modify: update echo instraction text

--- .config/conda/install_miniconda_helper.sh
+++ .config/conda/install_miniconda_helper.sh
@@ -3,5 +3,6 @@
 curl -O https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
 chmod a+x Miniconda3-latest-Linux-x86_64.sh
 
-echo "exec ./Miniconda3-latest-Linux-x86_64.sh"
+echo "To install miniconda, run below command"
+echo "./Miniconda3-latest-Linux-x86_64.sh"
 
