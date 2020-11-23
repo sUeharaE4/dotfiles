d2d948ca2e97181518836cc736eea7fe0d7d2a3f
sUeharaE4 <funa1hasi2hasi@gmail.com>
2020-11-23 21:24:22 +0900

    Add: insatll helper

--- .bin/install.sh
+++ .bin/install.sh
@@ -6,6 +6,19 @@ cd ${DOTFILE_DIR}
 EXEC_DATETIME=$(date +%Y%m%d_%H%M%S)
 BACKUP_DIR=${HOME}/backup_${EXEC_DATETIME}
 
+# check requirements
+if ! command -v curl &> /dev/null
+then
+  echo "curl could not be found. please install before exec this script"
+  exit
+fi
+
+if ! command -v git &> /dev/null
+then
+  echo "git could not be found. please install before exec this script"
+  exit
+fi
+
 echo "exec datetime: ${BACKUP_DIR}"
 echo "your dotfiles are copied to ${EXEC_DATETIME}"
 echo "please remove backup when you feel anything alright"
@@ -35,3 +48,15 @@ do
     done
   fi
 done
+
+if ! command -v fzf &> /dev/null
+then
+  echo "fzf could not be found. some git aliase needs fzf"
+  read -p "install fzf? [yes/no] >" install_fzf
+  if [ "${install_fzf}" in [Yy]|[Yy][Ee][Ss] ]; then
+    git clone https://github.com/junegunn/fzf.git ~/.fzf
+    ~/.fzf/install
+  fi
+fi
+
+
---
+++ .config/conda/install_miniconda_helper.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+curl -O https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
+chmod a+x Miniconda3-latest-Linux-x86_64.sh
+
+echo "exec ./Miniconda3-latest-Linux-x86_64.sh"
+
