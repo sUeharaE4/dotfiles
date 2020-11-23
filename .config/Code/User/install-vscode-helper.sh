2977632d369cdbf25e59af52c8ba8b0fa34b4129
sUeharaE4 <funa1hasi2hasi@gmail.com>
2020-11-24 08:22:08 +0900

    Modify: update echo instraction text

--- .config/Code/User/install-vscode-helper.sh
+++ .config/Code/User/install-vscode-helper.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 curl -L "https://go.microsoft.com/fwlink/?LinkID=760868"> vscode.deb
-sudo apt install ./vscode.deb
+echo "To install vscode, run below command."
+echo "sudo apt install ./vscode.deb"
 
