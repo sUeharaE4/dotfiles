13a81ae6b2ae22d45100920b0867a6052a9d18bb
sUeharaE4 <funa1hasi2hasi@gmail.com>
2020-12-26 15:01:06 +0900

    Modify: installer url

--- .config/conda/install_miniconda_helper.sh
+++ .config/conda/install_miniconda_helper.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl -O https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
+curl -O https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 chmod a+x Miniconda3-latest-Linux-x86_64.sh
 
 echo "To install miniconda, run below command"
