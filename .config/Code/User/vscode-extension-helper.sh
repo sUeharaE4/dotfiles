cfd93a9e73686666180c568c252c4ea43312e42c
sUeharaE4 <funa1hasi2hasi@gmail.com>
2022-07-03 00:48:46 +0900

    Fix: use option i to ignore case

--- .config/Code/User/vscode-extension-helper.sh
+++ .config/Code/User/vscode-extension-helper.sh
@@ -3,7 +3,7 @@
 cd "$(dirname "$0")"
 
 for ext in $(code --list-extensions); do
-  if ! grep -q $ext vscode-extension.sh; then
+  if ! grep -iq $ext vscode-extension.sh; then
     echo "this extension is not in pkglist of install shell: ${ext}";
   fi
 done
