78684f88ed4787cc988fbc9a97fa06e5da0013c1
sUeharaE4 <funa1hasi2hasi@gmail.com>
2022-07-03 00:40:16 +0900

    Add: create helper to list vscode extensions installed by GUI

---
+++ .config/Code/User/vscode-extension-helper.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd "$(dirname "$0")"
+
+for ext in $(code --list-extensions); do
+  if ! grep -q $ext vscode-extension.sh; then
+    echo "this extension is not in pkglist of install shell: ${ext}";
+  fi
+done
