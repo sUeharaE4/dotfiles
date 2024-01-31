ff77e444efadbf1a05f01c10411e4a1bcbff7549
sUeharaE4 <funa1hasi2hasi@gmail.com>
2024-01-31 11:31:41 +0900

    Uppdate: apply format

--- .config/nvim/lua/custom/plugins/java.lua
+++ .config/nvim/lua/custom/plugins/java.lua
@@ -1,10 +1,10 @@
 local function get_jdtls()
-  local mason_registry = require "mason-registry"
-  local jdtls = mason_registry.get_package "jdtls"
+  local mason_registry = require("mason-registry")
+  local jdtls = mason_registry.get_package("jdtls")
   local jdtls_path = jdtls:get_install_path()
   local launcher = vim.fn.glob(jdtls_path .. "/plugins/org.eclipse.equinox.launcher_*.jar")
   local SYSTEM = "linux"
-  if vim.fn.has "mac" == 1 then
+  if vim.fn.has("mac") == 1 then
     SYSTEM = "mac"
   end
   local config = jdtls_path .. "/config_" .. SYSTEM
@@ -13,20 +13,22 @@ local function get_jdtls()
 end
 
 local function get_bundles()
-  local mason_registry = require "mason-registry"
-  local java_debug = mason_registry.get_package "java-debug-adapter"
-  local java_test = mason_registry.get_package "java-test"
+  local mason_registry = require("mason-registry")
+  local java_debug = mason_registry.get_package("java-debug-adapter")
+  local java_test = mason_registry.get_package("java-test")
   local java_debug_path = java_debug:get_install_path()
   local java_test_path = java_test:get_install_path()
   local bundles = {}
-  vim.list_extend(bundles,
-    vim.split(vim.fn.glob(java_debug_path .. "/extension/server/com.microsoft.java.debug.plugin-*.jar"), "\n"))
+  vim.list_extend(
+    bundles,
+    vim.split(vim.fn.glob(java_debug_path .. "/extension/server/com.microsoft.java.debug.plugin-*.jar"), "\n")
+  )
   vim.list_extend(bundles, vim.split(vim.fn.glob(java_test_path .. "/extension/server/*.jar"), "\n"))
   return bundles
 end
 
 local function get_workspace()
-  local home = os.getenv "HOME"
+  local home = os.getenv("HOME")
   local workspace_path = home .. "/.local/share/nde/jdtls-workspace/"
   local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t")
   local workspace_dir = workspace_path .. project_name
@@ -67,7 +69,7 @@ return {
         pattern = { "java" },
         callback = function()
           -- LSP capabilities
-          local jdtls = require "jdtls"
+          local jdtls = require("jdtls")
           local capabilities = require("base.lsp.utils").capabilities()
           local extendedClientCapabilities = jdtls.extendedClientCapabilities
           extendedClientCapabilities.resolveAdditionalTextEditsSupport = true
@@ -78,7 +80,7 @@ return {
 
           local on_attach = function(_, bufnr)
             vim.lsp.codelens.refresh()
-            jdtls.setup_dap { hotcodereplace = "auto" }
+            jdtls.setup_dap({ hotcodereplace = "auto" })
             require("jdtls.dap").setup_dap_main_class_configs()
             require("jdtls.setup").add_commands()
 
@@ -86,11 +88,16 @@ return {
               if desc then
                 desc = desc
               end
-              vim.keymap.set(mode, lhs, rhs, { silent = true, desc = desc, buffer = bufnr, noremap = true })
+              vim.keymap.set(
+                mode,
+                lhs,
+                rhs,
+                { silent = true, desc = desc, buffer = bufnr, noremap = true }
+              )
             end
 
             -- Register keymappings
-            local wk = require "which-key"
+            local wk = require("which-key")
             local keys = { mode = { "n", "v" }, ["<leader>lj"] = { name = "+Java" } }
             wk.register(keys)
 
@@ -100,9 +107,24 @@ return {
             map("n", "<leader>ljt", jdtls.test_nearest_method, "Test Nearest Method")
             map("n", "<leader>ljT", jdtls.test_class, "Test Class")
             map("n", "<leader>lju", "<cmd>JdtUpdateConfig<cr>", "Update Config")
-            map("v", "<leader>ljv", "<esc><cmd>lua require('jdtls').extract_variable(true)<cr>", "Extract Variable")
-            map("v", "<leader>ljc", "<esc><cmd>lua require('jdtls').extract_constant(true)<cr>", "Extract Constant")
-            map("v", "<leader>ljm", "<esc><Cmd>lua require('jdtls').extract_method(true)<cr>", "Extract Method")
+            map(
+              "v",
+              "<leader>ljv",
+              "<esc><cmd>lua require('jdtls').extract_variable(true)<cr>",
+              "Extract Variable"
+            )
+            map(
+              "v",
+              "<leader>ljc",
+              "<esc><cmd>lua require('jdtls').extract_constant(true)<cr>",
+              "Extract Constant"
+            )
+            map(
+              "v",
+              "<leader>ljm",
+              "<esc><Cmd>lua require('jdtls').extract_method(true)<cr>",
+              "Extract Method"
+            )
 
             vim.api.nvim_create_autocmd("BufWritePost", {
               pattern = { "*.java" },
@@ -134,7 +156,13 @@ return {
               "-data",
               workspace_dir,
             },
-            root_dir = require("jdtls.setup").find_root { ".git", "mvnw", "gradlew", "pom.xml", "build.gradle" },
+            root_dir = require("jdtls.setup").find_root({
+              ".git",
+              "mvnw",
+              "gradlew",
+              "pom.xml",
+              "build.gradle",
+            }),
             capabilities = capabilities,
             on_attach = on_attach,
 
