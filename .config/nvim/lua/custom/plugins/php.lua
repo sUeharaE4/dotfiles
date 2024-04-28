13ad1d31388769c7533f30544230fc072f164e41
sUeharaE4 <44468359+sUeharaE4@users.noreply.github.com>
2024-04-28 15:09:29 +0900

    Update: add php libs (#4)

---
+++ .config/nvim/lua/custom/plugins/php.lua
@@ -0,0 +1,26 @@
+return {
+    {
+        "phpactor/phpactor",
+        dependencies = {
+            "nvim-lua/plenary.nvim", -- required to update phpactor
+            "neovim/nvim-lspconfig", -- required to automaticly register lsp serveur
+        },
+    },
+    {
+        "adalessa/laravel.nvim",
+        dependencies = {
+            "nvim-telescope/telescope.nvim",
+            "tpope/vim-dotenv",
+            "MunifTanjim/nui.nvim",
+            "nvimtools/none-ls.nvim",
+        },
+        cmd = { "Sail", "Artisan", "Composer", "Npm", "Yarn", "Laravel" },
+        keys = {
+            { "<leader>la", ":Laravel artisan<cr>" },
+            { "<leader>lr", ":Laravel routes<cr>" },
+            { "<leader>lm", ":Laravel related<cr>" },
+        },
+        event = { "VeryLazy" },
+        config = true,
+    },
+}
