df3b7a0bd48fb300d63b5fe03051060450e09690
sUeharaE4 <funa1hasi2hasi@gmail.com>
2025-06-26 22:21:27 +0900

    Update: setup keybindgs

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -42,7 +42,7 @@ config.font_size=16
 config.font = wezterm.font(
   -- "Moralerspace Argon HWNF",
   "JetBrains Mono",
-  { 
+  {
     stretch = 'Normal',
     weight = 'Regular',
     bold = false,
@@ -65,49 +65,6 @@ config.foreground_text_hsb = {
 }
 
 -- tab
-local tabline = wezterm.plugin.require("https://github.com/michaelbrusegard/tabline.wez")
-tabline.setup({
-    options = {
-        -- theme = "catppuccin-mocha",
-        theme = "cyberpunk",
-        -- theme = "Cobalt Neon",
-        section_separators = {
-            left = wezterm.nerdfonts.ple_upper_left_triangle,
-            right = wezterm.nerdfonts.ple_lower_right_triangle,
-        },
-        component_separators = {
-            left = wezterm.nerdfonts.ple_forwardslash_separator,
-            right = wezterm.nerdfonts.ple_forwardslash_separator,
-        },
-        tab_separators = {
-            left = wezterm.nerdfonts.ple_upper_left_triangle,
-            right = wezterm.nerdfonts.ple_lower_right_triangle,
-        },
-        -- color_overrides = {
-        theme_overrides = {
-            tab = {
-                active = { fg = "#091833", bg = "#59c2c6" },
-            },
-        },
-    },
-    sections = {
-        tab_active = {
-            "index",
-            { "process", padding = { left = 0, right = 1 } },
-            "",
-            { "cwd",     padding = { left = 1, right = 0 } },
-            { "zoomed",  padding = 1 },
-        },
-        tab_inactive = {
-            "index",
-            { "process", padding = { left = 0, right = 1 } },
-            "󰉋",
-            { "cwd",     padding = { left = 1, right = 0 } },
-            { "zoomed",  padding = 1 },
-        },
-    },
-})
-
 -- window related
 -- config.window_frame = {
 --   inactive_titlebar_bg = "#44475A",
@@ -187,6 +144,12 @@ config.window_background_gradient = {
   segment_size = 11,
   segment_smoothness = 1.0,
 }
+
+local keybind = require 'keybinds'
+-- config.disable_default_key_bindings = true
+config.keys = keybind.keys
+config.key_tables = keybind.key_tables
+config.leader = { key = "q", mods = "CTRL", timeout_milliseconds = 2000 }
 
 -- and finally, return the configuration to wezterm
 return config
