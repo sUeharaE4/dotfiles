756adf317627ee8582261119b86f9ec84d89d918
sUeharaE4 <funa1hasi2hasi@gmail.com>
2025-06-27 01:08:25 +0900

    Fix: remove fish

--- .config/nvim/lua/custom/plugins/none-ls.lua
+++ .config/nvim/lua/custom/plugins/none-ls.lua
@@ -36,8 +36,7 @@ return {
             opts.root_dir = opts.root_dir
                 or require("null-ls.utils").root_pattern(".null-ls-root", ".neoconf.json", "Makefile", ".git")
             opts.sources = vim.list_extend(opts.sources or {}, {
-                nls.builtins.formatting.fish_indent,
-                nls.builtins.diagnostics.fish,
+                
                 nls.builtins.formatting.stylua,
                 nls.builtins.formatting.shfmt,
                 nls.builtins.formatting.terraform_fmt,
