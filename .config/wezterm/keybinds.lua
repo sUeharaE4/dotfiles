528f39c5b5330d4bf3858afd9a316f18d5ec8668
sUeharaE4 <44468359+sUeharaE4@users.noreply.github.com>
2025-06-26 22:57:50 +0900

    Add Wezterm (#12)
    * Add: create config for wezterm
    
    * Add: default keybindings
    
    * Update: setup keybindgs
    
    * Update: remove unneed buttom
    
    * Update: make simple
    
    * Update: make command to jump words

---
+++ .config/wezterm/keybinds.lua
@@ -0,0 +1,173 @@
+local wezterm = require("wezterm")
+local act = wezterm.action
+
+-- Show which key table is active in the status area
+wezterm.on("update-right-status", function(window, pane)
+  local name = window:active_key_table()
+  if name then
+    name = "TABLE: " .. name
+  end
+  window:set_right_status(name or "")
+end)
+
+return {
+  keys = {
+    {
+      -- switch workspace
+      key = "w",
+      mods = "LEADER",
+      action = act.ShowLauncherArgs({ flags = "WORKSPACES", title = "Select workspace" }),
+    },
+    {
+      -- rename workspace
+      key = "$",
+      mods = "LEADER",
+      action = act.PromptInputLine({
+        description = "(wezterm) Set workspace title:",
+        action = wezterm.action_callback(function(win, pane, line)
+          if line then
+            wezterm.mux.rename_workspace(wezterm.mux.get_active_workspace(), line)
+          end
+        end),
+      }),
+    },
+    {
+      -- create workspace
+      key = "W",
+      mods = "LEADER|SHIFT",
+      action = act.PromptInputLine({
+        description = "(wezterm) Create new workspace:",
+        action = wezterm.action_callback(function(window, pane, line)
+          if line then
+            window:perform_action(
+              act.SwitchToWorkspace({
+                name = line,
+              }),
+              pane
+            )
+          end
+        end),
+      }),
+    },
+    -- tab
+    { key = "Tab", mods = "CTRL", action = act.ActivateTabRelative(1) },
+    { key = "Tab", mods = "SHIFT|CTRL", action = act.ActivateTabRelative(-1) },
+    { key = "{", mods = "LEADER", action = act({ MoveTabRelative = -1 }) },
+    { key = "t", mods = "SUPER", action = act({ SpawnTab = "CurrentPaneDomain" }) },
+    { key = "w", mods = "SUPER", action = act({ CloseCurrentTab = { confirm = true } }) },
+    { key = "}", mods = "LEADER", action = act({ MoveTabRelative = 1 }) },
+
+    -- fullscreen
+    { key = "Enter", mods = "ALT", action = act.ToggleFullScreen },
+
+    -- copy mode
+    { key = "v", mods = "LEADER", action = act.ActivateCopyMode },
+    -- yank
+    { key = "y", mods = "SUPER", action = act.CopyTo("Clipboard") },
+    -- paste
+    { key = "p", mods = "SUPER", action = act.PasteFrom("Clipboard") },
+
+    -- create pane
+    { key = "d", mods = "LEADER", action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
+    { key = "D", mods = "LEADER", action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+    -- close pane
+    { key = "x", mods = "LEADER", action = act({ CloseCurrentPane = { confirm = true } }) },
+    -- move to other pane
+    { key = "h", mods = "LEADER", action = act.ActivatePaneDirection("Left") },
+    { key = "l", mods = "LEADER", action = act.ActivatePaneDirection("Right") },
+    { key = "k", mods = "LEADER", action = act.ActivatePaneDirection("Up") },
+    { key = "j", mods = "LEADER", action = act.ActivatePaneDirection("Down") },
+    -- select pane
+    { key = "[", mods = "CTRL|SHIFT", action = act.PaneSelect },
+    -- Only show current pane
+    { key = "z", mods = "LEADER", action = act.TogglePaneZoomState },
+
+    -- change font size
+    { key = "+", mods = "CTRL", action = act.IncreaseFontSize },
+    { key = "-", mods = "CTRL", action = act.DecreaseFontSize },
+    { key = "0", mods = "CTRL", action = act.ResetFontSize },
+
+    -- switch tab
+    { key = "1", mods = "SUPER", action = act.ActivateTab(0) },
+    { key = "2", mods = "SUPER", action = act.ActivateTab(1) },
+    { key = "3", mods = "SUPER", action = act.ActivateTab(2) },
+    { key = "4", mods = "SUPER", action = act.ActivateTab(3) },
+    { key = "5", mods = "SUPER", action = act.ActivateTab(4) },
+    { key = "6", mods = "SUPER", action = act.ActivateTab(5) },
+    { key = "7", mods = "SUPER", action = act.ActivateTab(6) },
+    { key = "8", mods = "SUPER", action = act.ActivateTab(7) },
+    { key = "9", mods = "SUPER", action = act.ActivateTab(-1) },
+
+    -- resize pane
+    { key = "s", mods = "LEADER", action = act.ActivateKeyTable({ name = "resize_pane", one_shot = false }) },
+    {
+      key = "a",
+      mods = "LEADER",
+      action = act.ActivateKeyTable({ name = "activate_pane", timeout_milliseconds = 1000 }),
+    },
+    -- create key to jump words in terminal
+    {key="LeftArrow", mods="OPT", action=wezterm.action{SendString="\x1bb"}},
+    {key="RightArrow", mods="OPT", action=wezterm.action{SendString="\x1bf"}},
+  },
+
+  -- https://wezfurlong.org/wezterm/config/key-tables.html
+  key_tables = {
+    -- resize with leader + s
+    resize_pane = {
+      { key = "h", action = act.AdjustPaneSize({ "Left", 1 }) },
+      { key = "l", action = act.AdjustPaneSize({ "Right", 1 }) },
+      { key = "k", action = act.AdjustPaneSize({ "Up", 1 }) },
+      { key = "j", action = act.AdjustPaneSize({ "Down", 1 }) },
+
+      -- Cancel the mode by pressing escape
+      { key = "Enter", action = "PopKeyTable" },
+    },
+    activate_pane = {
+      { key = "h", action = act.ActivatePaneDirection("Left") },
+      { key = "l", action = act.ActivatePaneDirection("Right") },
+      { key = "k", action = act.ActivatePaneDirection("Up") },
+      { key = "j", action = act.ActivatePaneDirection("Down") },
+    },
+    -- copy mode + [
+    copy_mode = {
+      { key = "h", mods = "NONE", action = act.CopyMode("MoveLeft") },
+      { key = "j", mods = "NONE", action = act.CopyMode("MoveDown") },
+      { key = "k", mods = "NONE", action = act.CopyMode("MoveUp") },
+      { key = "l", mods = "NONE", action = act.CopyMode("MoveRight") },
+      { key = "^", mods = "NONE", action = act.CopyMode("MoveToStartOfLineContent") },
+      { key = "$", mods = "NONE", action = act.CopyMode("MoveToEndOfLineContent") },
+      { key = "0", mods = "NONE", action = act.CopyMode("MoveToStartOfLine") },
+      { key = "w", mods = "NONE", action = act.CopyMode("MoveForwardWord") },
+      { key = "b", mods = "NONE", action = act.CopyMode("MoveBackwardWord") },
+      { key = "e", mods = "NONE", action = act.CopyMode("MoveForwardWordEnd") },
+      { key = "t", mods = "NONE", action = act.CopyMode({ JumpForward = { prev_char = true } }) },
+      { key = "f", mods = "NONE", action = act.CopyMode({ JumpForward = { prev_char = false } }) },
+      { key = "T", mods = "NONE", action = act.CopyMode({ JumpBackward = { prev_char = true } }) },
+      { key = "F", mods = "NONE", action = act.CopyMode({ JumpBackward = { prev_char = false } }) },
+      { key = "G", mods = "NONE", action = act.CopyMode("MoveToScrollbackBottom") },
+      { key = "g", mods = "NONE", action = act.CopyMode("MoveToScrollbackTop") },
+      { key = "H", mods = "NONE", action = act.CopyMode("MoveToViewportTop") },
+      { key = "L", mods = "NONE", action = act.CopyMode("MoveToViewportBottom") },
+      { key = "M", mods = "NONE", action = act.CopyMode("MoveToViewportMiddle") },
+      { key = "b", mods = "CTRL", action = act.CopyMode("PageUp") },
+      { key = "f", mods = "CTRL", action = act.CopyMode("PageDown") },
+      { key = "d", mods = "CTRL", action = act.CopyMode({ MoveByPage = 0.5 }) },
+      { key = "u", mods = "CTRL", action = act.CopyMode({ MoveByPage = -0.5 }) },
+      { key = "v", mods = "NONE", action = act.CopyMode({ SetSelectionMode = "Cell" }) },
+      { key = "v", mods = "CTRL", action = act.CopyMode({ SetSelectionMode = "Block" }) },
+      { key = "V", mods = "NONE", action = act.CopyMode({ SetSelectionMode = "Line" }) },
+      { key = "y", mods = "NONE", action = act.CopyTo("Clipboard") },
+
+      -- Cancel the mode by pressing enter
+      {
+        key = "Enter",
+        mods = "NONE",
+        action = act.Multiple({ { CopyTo = "ClipboardAndPrimarySelection" }, { CopyMode = "Close" } }),
+      },
+      { key = "Escape", mods = "NONE", action = act.CopyMode("Close") },
+      { key = "c", mods = "CTRL", action = act.CopyMode("Close") },
+      { key = "q", mods = "NONE", action = act.CopyMode("Close") },
+    },
+  },
+}
+
---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,159 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices
+
+-- For example, changing the color scheme:
+config.color_scheme = 'AdventureTime'
+
+
+-- config should be reload when the config file changes
+config.automatically_reload_config = true
+wezterm.on('window-config-reloaded', function(window, pane)
+  wezterm.log_info 'the config was reloaded for this window!'
+end)
+
+-- update
+config.check_for_updates = true
+config.check_for_updates_interval_seconds = 86400
+
+-- scroll backline
+config.scrollback_lines = 3000
+
+-- ime
+config.use_ime = true
+
+-- exit
+config.exit_behavior = 'CloseOnCleanExit'
+
+
+-- This is where you actually apply your config choices
+config.color_scheme = "Vs Code Dark+ (Gogh)"
+
+-- background
+config.window_background_opacity = 0.95
+config.macos_window_background_blur = 10
+
+-- font related
+config.font_size=16
+config.font = wezterm.font(
+  -- "Moralerspace Argon HWNF",
+  "JetBrains Mono",
+  {
+    stretch = 'Normal',
+    weight = 'Regular',
+    bold = false,
+    italic = false,
+  }
+)
+config.harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' }
+config.text_background_opacity = 0.95
+config.font_size = 16
+config.cell_width = 1.0
+config.line_height = 1.0
+config.use_cap_height_to_scale_fallback_fonts = true
+
+
+-- This increases color saturation by 50%
+config.foreground_text_hsb = {
+ hue = 1.0,
+ saturation = 1.0,
+ brightness = 1.2,
+}
+
+-- tab
+config.show_new_tab_button_in_tab_bar = false
+config.show_close_tab_button_in_tabs = false
+
+-- window related
+-- config.window_decorations = "RESIZE"
+-- config.window_frame = {
+--   inactive_titlebar_bg = "#44475A",
+--   active_titlebar_bg = "#BD93F9",
+--   inactive_titlebar_fg = "#44475A",
+--   active_titlebar_fg = "#F8F8F2",
+--   inactive_titlebar_border_bottom = "#282A36",
+--   active_titlebar_border_bottom = "#282A36",
+--   button_fg = "#44475A",
+--   button_bg = "#282A36",
+--   button_hover_fg = "#F8F8F2",
+--   button_hover_bg = "#282A36",
+-- }
+config.window_padding = {
+ left = 5,
+ right = 5,
+ top = 10,
+ bottom = 5,
+}
+config.window_background_gradient = {
+  -- Can be "Vertical" or "Horizontal".  Specifies the direction
+  -- in which the color gradient varies.  The default is "Horizontal",
+  -- with the gradient going from left-to-right.
+  -- Linear and Radial gradients are also supported; see the other
+  -- examples below
+  -- orientation = "Vertical",
+  orientation = { Linear = { angle = -50.0 } },
+
+  -- Specifies the set of colors that are interpolated in the gradient.
+  -- Accepts CSS style color specs, from named colors, through rgb
+  -- strings and more
+  -- colors = {
+  --      "#0f0c29",
+  --      "#302b63",
+  --      "#24243e",
+  --},
+  colors = {
+    "#0f0c29",
+    "#282a36",
+    "#343746",
+    "#3a3f52",
+    "#343746",
+    "#282a36",
+  },
+  -- colors = { "Inferno" },
+
+  -- Instead of specifying `colors`, you can use one of a number of
+  -- predefined, preset gradients.
+  -- A list of presets is shown in a section below.
+  -- preset = "Warm",
+
+  -- Specifies the interpolation style to be used.
+  -- "Linear", "Basis" and "CatmullRom" as supported.
+  -- The default is "Linear".
+  interpolation = "Linear",
+
+  -- How the colors are blended in the gradient.
+  -- "Rgb", "LinearRgb", "Hsv" and "Oklab" are supported.
+  -- The default is "Rgb".
+  blend = "Rgb",
+
+  -- To avoid vertical color banding for horizontal gradients, the
+  -- gradient position is randomly shifted by up to the `noise` value
+  -- for each pixel.
+  -- Smaller values, or 0, will make bands more prominent.
+  -- The default value is 64 which gives decent looking results
+  -- on a retina macbook pro display.
+  noise = 64,
+
+  -- By default, the gradient smoothly transitions between the colors.
+  -- You can adjust the sharpness by specifying the segment_size and
+  -- segment_smoothness parameters.
+  -- segment_size configures how many segments are present.
+  -- segment_smoothness is how hard the edge is; 0.0 is a hard edge,
+  -- 1.0 is a soft edge.
+
+  segment_size = 11,
+  segment_smoothness = 1.0,
+}
+
+local keybind = require 'keybinds'
+-- config.disable_default_key_bindings = true
+config.keys = keybind.keys
+config.key_tables = keybind.key_tables
+config.leader = { key = "q", mods = "CTRL", timeout_milliseconds = 2000 }
+
+-- and finally, return the configuration to wezterm
+return config
