2a4497eb6c9ff2a0341549c74b85ce5caeebbe47
sUeharaE4 <funa1hasi2hasi@gmail.com>
2025-11-03 01:07:35 +0900

    Update: align which-key registration with v2 spec

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -522,23 +522,15 @@ end
 -- })
 
 require("which-key").register({
-  { "<leader>c",  group = "[C]ode" },
-  { "<leader>c_", hidden = true },
-  { "<leader>d",  group = "[D]ocument" },
-  { "<leader>d_", hidden = true },
-  { "<leader>g",  group = "[G]it" },
-  { "<leader>g_", hidden = true },
-  { "<leader>h",  group = "Git [H]unk" },
-  { "<leader>h_", hidden = true },
-  { "<leader>r",  group = "[R]ename" },
-  { "<leader>r_", hidden = true },
-  { "<leader>s",  group = "[S]earch" },
-  { "<leader>s_", hidden = true },
-  { "<leader>t",  group = "[T]oggle" },
-  { "<leader>t_", hidden = true },
-  { "<leader>w",  group = "[W]orkspace" },
-  { "<leader>w_", hidden = true },
-})
+  { "<leader>c", group = "[C]ode" },
+  { "<leader>d", group = "[D]ocument" },
+  { "<leader>g", group = "[G]it" },
+  { "<leader>h", group = "Git [H]unk" },
+  { "<leader>r", group = "[R]ename" },
+  { "<leader>s", group = "[S]earch" },
+  { "<leader>t", group = "[T]oggle" },
+  { "<leader>w", group = "[W]orkspace" },
+}, { mode = "n" })
 -- register which-key VISUAL mode
 -- required for visual <leader>hs (hunk stage) to work
 -- require("which-key").register({
@@ -547,8 +539,8 @@ require("which-key").register({
 -- }, { mode = "v" })
 
 require("which-key").register({
-  { "<leader>",  group = "VISUAL <leader>", mode = "v" },
-  { "<leader>h", desc = "Git [H]unk",       mode = "v" },
+  { "<leader>", group = "VISUAL <leader>", mode = "v" },
+  { "<leader>h", desc = "Git [H]unk", mode = "v" },
 })
 
 -- Setup neovim lua configuration
