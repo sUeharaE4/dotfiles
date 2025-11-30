857ae49571b24af6eb9518e10f7f527d4e79a926
sUeharaE4 <funa1hasi2hasi@gmail.com>
2025-11-30 16:38:39 +0900

    Update: fmt

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,5 +1,5 @@
 -- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 -- This will hold the configuration.
 local config = wezterm.config_builder()
@@ -7,13 +7,12 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
-config.color_scheme = 'AdventureTime'
-
+config.color_scheme = "AdventureTime"
 
 -- config should be reload when the config file changes
 config.automatically_reload_config = true
-wezterm.on('window-config-reloaded', function(window, pane)
-  wezterm.log_info 'the config was reloaded for this window!'
+wezterm.on("window-config-reloaded", function(window, pane)
+    wezterm.log_info("the config was reloaded for this window!")
 end)
 
 -- update
@@ -27,8 +26,7 @@ config.scrollback_lines = 3000
 config.use_ime = true
 
 -- exit
-config.exit_behavior = 'CloseOnCleanExit'
-
+config.exit_behavior = "CloseOnCleanExit"
 
 -- This is where you actually apply your config choices
 config.color_scheme = "Vs Code Dark+ (Gogh)"
@@ -43,30 +41,29 @@ config.inactive_pane_hsb = {
 }
 
 -- font related
-config.font_size=16
+config.font_size = 16
 config.font = wezterm.font(
-  -- "Moralerspace Argon HWNF",
-  "JetBrains Mono",
-  {
-    stretch = 'Normal',
-    weight = 'Regular',
-    bold = false,
-    italic = false,
-  }
+-- "Moralerspace Argon HWNF",
+    "JetBrains Mono",
+    {
+        stretch = "Normal",
+        weight = "Regular",
+        bold = false,
+        italic = false,
+    }
 )
-config.harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' }
+config.harfbuzz_features = { "calt=0", "clig=0", "liga=0" }
 config.text_background_opacity = 0.95
 config.font_size = 16
 config.cell_width = 1.0
 config.line_height = 1.0
 config.use_cap_height_to_scale_fallback_fonts = true
 
-
 -- This increases color saturation by 50%
 config.foreground_text_hsb = {
- hue = 1.0,
- saturation = 1.0,
- brightness = 1.2,
+    hue = 1.0,
+    saturation = 1.0,
+    brightness = 1.2,
 }
 
 -- tab
@@ -88,73 +85,73 @@ config.show_close_tab_button_in_tabs = false
 --   button_hover_bg = "#282A36",
 -- }
 config.window_padding = {
- left = 5,
- right = 5,
- top = 10,
- bottom = 5,
+    left = 5,
+    right = 5,
+    top = 10,
+    bottom = 5,
 }
 config.window_background_gradient = {
-  -- Can be "Vertical" or "Horizontal".  Specifies the direction
-  -- in which the color gradient varies.  The default is "Horizontal",
-  -- with the gradient going from left-to-right.
-  -- Linear and Radial gradients are also supported; see the other
-  -- examples below
-  -- orientation = "Vertical",
-  orientation = { Linear = { angle = -50.0 } },
+    -- Can be "Vertical" or "Horizontal".  Specifies the direction
+    -- in which the color gradient varies.  The default is "Horizontal",
+    -- with the gradient going from left-to-right.
+    -- Linear and Radial gradients are also supported; see the other
+    -- examples below
+    -- orientation = "Vertical",
+    orientation = { Linear = { angle = -50.0 } },
 
-  -- Specifies the set of colors that are interpolated in the gradient.
-  -- Accepts CSS style color specs, from named colors, through rgb
-  -- strings and more
-  -- colors = {
-  --      "#0f0c29",
-  --      "#302b63",
-  --      "#24243e",
-  --},
-  colors = {
-    "#0f0c29",
-    "#282a36",
-    "#343746",
-    "#3a3f52",
-    "#343746",
-    "#282a36",
-  },
-  -- colors = { "Inferno" },
+    -- Specifies the set of colors that are interpolated in the gradient.
+    -- Accepts CSS style color specs, from named colors, through rgb
+    -- strings and more
+    -- colors = {
+    --      "#0f0c29",
+    --      "#302b63",
+    --      "#24243e",
+    --},
+    colors = {
+        "#0f0c29",
+        "#282a36",
+        "#343746",
+        "#3a3f52",
+        "#343746",
+        "#282a36",
+    },
+    -- colors = { "Inferno" },
 
-  -- Instead of specifying `colors`, you can use one of a number of
-  -- predefined, preset gradients.
-  -- A list of presets is shown in a section below.
-  -- preset = "Warm",
+    -- Instead of specifying `colors`, you can use one of a number of
+    -- predefined, preset gradients.
+    -- A list of presets is shown in a section below.
+    -- preset = "Warm",
 
-  -- Specifies the interpolation style to be used.
-  -- "Linear", "Basis" and "CatmullRom" as supported.
-  -- The default is "Linear".
-  interpolation = "Linear",
+    -- Specifies the interpolation style to be used.
+    -- "Linear", "Basis" and "CatmullRom" as supported.
+    -- The default is "Linear".
+    interpolation = "Linear",
 
-  -- How the colors are blended in the gradient.
-  -- "Rgb", "LinearRgb", "Hsv" and "Oklab" are supported.
-  -- The default is "Rgb".
-  blend = "Rgb",
+    -- How the colors are blended in the gradient.
+    -- "Rgb", "LinearRgb", "Hsv" and "Oklab" are supported.
+    -- The default is "Rgb".
+    blend = "Rgb",
 
-  -- To avoid vertical color banding for horizontal gradients, the
-  -- gradient position is randomly shifted by up to the `noise` value
-  -- for each pixel.
-  -- Smaller values, or 0, will make bands more prominent.
-  -- The default value is 64 which gives decent looking results
-  -- on a retina macbook pro display.
-  noise = 64,
+    -- To avoid vertical color banding for horizontal gradients, the
+    -- gradient position is randomly shifted by up to the `noise` value
+    -- for each pixel.
+    -- Smaller values, or 0, will make bands more prominent.
+    -- The default value is 64 which gives decent looking results
+    -- on a retina macbook pro display.
+    noise = 64,
 
-  -- By default, the gradient smoothly transitions between the colors.
-  -- You can adjust the sharpness by specifying the segment_size and
-  -- segment_smoothness parameters.
-  -- segment_size configures how many segments are present.
-  -- segment_smoothness is how hard the edge is; 0.0 is a hard edge,
-  -- 1.0 is a soft edge.
+    -- By default, the gradient smoothly transitions between the colors.
+    -- You can adjust the sharpness by specifying the segment_size and
+    -- segment_smoothness parameters.
+    -- segment_size configures how many segments are present.
+    -- segment_smoothness is how hard the edge is; 0.0 is a hard edge,
+    -- 1.0 is a soft edge.
 
-  segment_size = 11,
-  segment_smoothness = 1.0,
+    segment_size = 11,
+    segment_smoothness = 1.0,
 }
 
-local keybind = require 'keybinds'
+local keybind = require("keybinds")
 -- config.disable_default_key_bindings = true
 config.keys = keybind.keys
 config.key_tables = keybind.key_tables
